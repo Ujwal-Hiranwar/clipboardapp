5b33b9aeb8970ce93e48c3fa0af68cb3184e91f9
Ujjwal-Hiranwar <ujjwalhiranwar15@gmail.com>
2025-02-16 08:52:08 +0000

    aded

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@
 
 import Navbar from './components/Navbar'
 import SendTextArea from './components/SendTextArea'
-import ReciveText from './components/ReciveText'
+
 import { StreamlinedClipboard } from './components/streamline-clipboard';
 export default function page(props) {
 
